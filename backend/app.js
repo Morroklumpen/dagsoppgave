ed0d42ab04d62d50b2f8af81dd49004403124089
morroklumpen <tord.rustand@gmail.com>
2024-04-15 12:26:24 +0200

    jhgf
    fjgh

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,4 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const bcrypt = require('bcrypt');
 
- 
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(cors());
- 
-// // MongoDB connection URI
-// const uri = 'mongodb://10.12.14.42:27017/?directConnection=true&appName=mongosh+2.2.0';
- 
-// // Connect to MongoDB
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- 
-// // Create a connection instance
-// const db = mongoose.connection;
- 
-// // Event handlers for successful and failed connections
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//   console.log('Server connected to MongoDB');
-// });
- 
 
 // server.js (backend)
 const express = require('express');
@@ -103,4 +78,3 @@ app.listen(PORT, () => {
 });
 
 
-  
